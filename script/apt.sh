e8332c0191c2deacfaa5aaec10e654b8bf78e4ab
Orkun Şensebat <ihate88@gmail.com>
2014-02-20 07:13:17 +0100

    Update apt.sh

--- script/apt.sh
+++ script/apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get insitall vim-scripts exuberant-ctags vim-latexsuite -y
+#apt-get install vim-scripts exuberant-ctags vim-latexsuite -y
 #vim-addons install latex-suite
 #vim-addons install omnicppcomplete
 #vim-addons install taglist

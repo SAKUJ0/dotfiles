ea7dd6d9ecf4c84623e267f51e7873893b8fe6b2
Orkun Şensebat <ihate88@gmail.com>
2014-02-20 06:55:51 +0100

    vim-addons instal xyz

--- script/apt.sh
+++ script/apt.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-apt-get install vim-scripts exuberant-ctags vim-latexsuite
+apt-get insitall vim-scripts exuberant-ctags vim-latexsuite -y
+vim-addons install latex-suite
+vim-addons install omnicppcomplete
+vim-addons install taglist

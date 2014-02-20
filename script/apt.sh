c1ccf8a71126eea51cea67d5ed220999238cb9d8
Orkun Şensebat <ihate88@gmail.com>
2014-02-20 07:06:34 +0100

    Using local vim-addons installations

--- script/apt.sh
+++ script/apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get insitall vim-scripts exuberant-ctags vim-latexsuite -y
-vim-addons install latex-suite
-vim-addons install omnicppcomplete
-vim-addons install taglist
+#vim-addons install latex-suite
+#vim-addons install omnicppcomplete
+#vim-addons install taglist
